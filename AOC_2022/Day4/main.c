88e9c18446631aeb16ddcbfbdccd0251494f1a45
mariano <root@Mar>
2023-11-24 20:22:44 -0600

    AOC 22 Day 4 part 2 and clean up

--- AOC_2022/Day4/main.c
+++ AOC_2022/Day4/main.c
@@ -10,12 +10,8 @@ void reset_num(char arr[])
   arr[2] = '\0';
 }
 
-int main()
+void part_one(FILE *f)
 {
-  FILE *f;
-  f = fopen("input.txt","r");
-
-
   char buf[100];
   int row[4];
   int rowi = 0;
@@ -56,20 +52,12 @@ int main()
         //  A 11: 46-69,7-46
         if(row[0] <= row[2] && row[1] >= row[3])
         {
-          if(11 == count)
-          {
-    printf("\n A %d: %s\n",count,buf);
-            printf("->  %d,%d,%d,%d\n",row[0] , row[2] , row[1],  row[3]);
-            return 0;
-          }
           sum++;
-    printf("\n A %d: %s\n",count,buf);
         }
         // 2-3,1-3
         else if(row[2] <= row[0] && row[3] >= row[1])
         {
           sum++;
-    printf("\n B %d: %s\n",count,buf);
         }
       }
       else
@@ -83,8 +71,104 @@ int main()
   }
 
   printf("\nResult: %d\n",sum);
+}
+
+void part_two(FILE *f)
+{
+  char buf[100];
+  int row[4];
+  int rowi = 0;
+  int sum = 0;
+  int numi = 0;
+
+  int count = 0;
+
+
+  while(fgets(buf,100,f))
+  {
+    count++;
+    char num[3];
+    char len = strlen(buf);
+    for(int i = 0; i < len; i++)
+    {
+      if(buf[i] == ',')
+      {
+        row[rowi] = atoi(num);
+        reset_num(num);
+        rowi++;
+        numi=0;
+      }
+      else if(buf[i] == '-')
+      {
+        row[rowi] = atoi(num);
+        reset_num(num);
+        rowi++;
+        numi=0;
+      }
+      else if(buf[i] == '\n')
+      {
+        row[rowi] = atoi(num);
+        reset_num(num);
+        numi=0;
+        rowi++;
+        // 1-4,2-5
+        if(row[0] <= row[2] && row[1] >= row[2])
+        {
+          sum++;
+        }
+        // 2-6,4-5
+        else if(row[0] <= row[3] && row[1] >= row[3])
+        {
+          sum++;
+        }
+        else if(row[0] <= row[2] && row[1] >= row[3])
+        {
+          sum++;
+        }
+        // 2-3,1-3
+        else if(row[2] <= row[0] && row[3] >= row[1])
+        {
+          sum++;
+        }
+      }
+      else
+      {
+        num[numi] = buf[i];
+        numi++;
+      }
+    }
+    //reset for next row
+    rowi = 0;
+  }
+
+  printf("\nResult: %d\n",sum);
+}
+
+int main()
+{
+  FILE *f;
+  char c;
+  printf("enter 1 or 2 for the desire part: ");
+  c = getchar();
+
+  if(c == '1' || c == '2')
+  {
+    f = fopen("input.txt","r");
+    if(c == '1')
+    {
+      part_one(f);
+    }
+    else 
+    {
+      part_two(f);
+    }
+    fclose(f);
+  }
+  else 
+  {
+    printf("\ninvalid option!!!!\n");
+  }
   
   
-  fclose(f);
   return 0;
 }
